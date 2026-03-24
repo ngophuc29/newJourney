5ed9dbb95615fc238265ff919326ff537c5aa4b2
ngophuc29 <ngophuc2911@gmail.com>
2026-03-24 21:31:56 +0700

    setup shadcn va lam ui cho dky va dnhap

---
+++ frontend/src/components/auth/signin-form.tsx
@@ -0,0 +1,136 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "../ui/label"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+const signInSchema = z.object({
+  firstname: z.string().min(1, "Ten bat buoc phai co"),
+  lastname: z.string().min(1, "Ho bat buoc phai co"),
+  username: z.string().min(3, "Ten dang nhap co it nhat 3 ki tu"),
+  email: z.email("Email k hop le"),
+  password: z.string().min(6, "Password toi thieu 6 ki tu")
+
+
+})
+
+type SignInFormValues = z.infer<typeof signInSchema>
+
+export function SigninForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) { 
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<SignInFormValues>({
+      resolver: zodResolver(signInSchema)
+    });
+  
+  
+  
+    const onSubmit = async (data: SignInFormValues) => {
+  
+      // goi api backend
+  
+    }
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              {/* header logo */}
+              <div className="flex flex-col items-center text-center gap-2">
+
+                <a href="/"
+                  className="mx-auto block w-fit text-center"
+                >
+                  <img src="/logo.svg" alt="logo" />
+
+                </a>
+
+                <h1 className="text-2xl font-bold"> Chao mung ban quay lai</h1>
+                <p className="text-muted-foreground text-balance">
+                  Chao mung ban
+                </p>
+              </div>
+               
+              {/* username */}
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="username"
+                    className="text-sm block"
+                  >Ten dang nhap</Label>
+
+                  <Input
+                    type="text"
+                    id="username"
+                    placeholder="phucngo"
+                    {...register("username")}
+
+                  />
+                  {errors.username && (
+                    <p className="text-destructive text-sm">
+                      {errors.username.message}
+                    </p>
+                  )}
+                </div>
+
+
+              </div>
+               
+              {/* password */}
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="password"
+                    className="text-sm block">
+                    Password
+                  </Label>
+
+                  <Input
+                    type="text"
+                    id="password"
+                    placeholder="phucngo"
+                    {...register("password")}
+
+                  />
+                  {errors.password && (
+                    <p className="text-destructive text-sm">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              {/* nut dang nhap*/}
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isSubmitting}
+              >Dang nhap
+              </Button>
+
+              <div className="text-center text-sm">
+                Chua co tai khoan <a href="/signup"
+                  className="underline underline-offset-4"
+                >Dang ki</a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/placeholder.png"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/components/auth/signup-form.tsx
@@ -0,0 +1,195 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "../ui/label"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+const signUpSchema = z.object({
+  firstname: z.string().min(1, "Ten bat buoc phai co"),
+  lastname: z.string().min(1, "Ho bat buoc phai co"),
+  username: z.string().min(3, "Ten dang nhap co it nhat 3 ki tu"),
+  email: z.email("Email k hop le"),
+  password: z.string().min(6, "Password toi thieu 6 ki tu")
+
+
+})
+
+type SignUpFormValues = z.infer<typeof signUpSchema>
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<SignUpFormValues>({
+    resolver: zodResolver(signUpSchema)
+  });
+
+
+
+  const onSubmit = async (data: SignUpFormValues) => {
+
+    // goi api backend
+
+  }
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              {/* header logo */}
+              <div className="flex flex-col items-center text-center gap-2">
+
+                <a href="/"
+                  className="mx-auto block w-fit text-center"
+                >
+                  <img src="/logo.svg" alt="logo" />
+
+                </a>
+
+                <h1 className="text-2xl font-bold"> Tao tai khoan</h1>
+                <p className="text-muted-foreground text-balance">
+                  Chao mung ban
+                </p>
+              </div>
+              {/* ho va ten */}
+              <div className="grid grid-cols-2 gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="lastName"
+                    className="text-sm block"
+                  >Ho</Label>
+
+                  <Input
+                    type="text"
+                    id="lastname"
+                    {...register("lastname")}
+                  />
+
+                  {errors.lastname && (
+                    <p className="text-destructive text-sm">
+                      { errors.lastname.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="space-y-2">
+                  <Label htmlFor="firstname"
+                    className="text-sm block"
+                  >Ten</Label>
+
+                  <Input
+                    type="text"
+                    id="firstname"
+                    {...register("firstname")}
+
+                  />
+                  {errors.firstname && (
+                    <p className="text-destructive text-sm">
+                      {errors.firstname.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              {/* username */}
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="username"
+                    className="text-sm block"
+                  >Ten dang nhap</Label>
+
+                  <Input
+                    type="text"
+                    id="username"
+                    placeholder="phucngo"
+                    {...register("username")}
+
+                  />
+                  {errors.username && (
+                    <p className="text-destructive text-sm">
+                      {errors.username.message}
+                    </p>
+                  )}
+                </div>
+
+
+              </div>
+              {/* email */}
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="email"
+                    className="text-sm block"
+                  >Email</Label>
+
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="m@gmail.com"
+                    {...register("email")}
+
+                  />
+                  {errors.email && (
+                    <p className="text-destructive text-sm">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+
+
+              </div>
+              {/* password */}
+              <div className="flex flex-col gap-3">
+                <div className="space-y-2">
+                  <Label htmlFor="password"
+                    className="text-sm block">
+                    Password
+                  </Label>
+
+                  <Input
+                    type="text"
+                    id="password"
+                    placeholder="phucngo"
+                    {...register("password")}
+
+                  />
+                  {errors.password && (
+                    <p className="text-destructive text-sm">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              {/* nut dang ky */}
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isSubmitting}
+              >Tao tai khaon
+              </Button>
+
+              <div className="text-center text-sm">
+                Da co tai khoan <a href="/signin"
+                  className="underline underline-offset-4"
+                >Dang nhap</a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
